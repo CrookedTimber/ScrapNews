356c53a16d6248a780999bba205d98a2d382edad
Edgar Ruizcd <45479530+CrookedTimber@users.noreply.github.com>
2022-10-25 15:18:39 +0100

    Ready for Deployment

---
+++ static/section_dictionaries.py
@@ -0,0 +1,121 @@
+headlines_dict = {
+    "aj": {"url": "https://www.aljazeera.com/news/", "tags": ["article", "gc"]},
+    "ib": {"url": "https://www.infobae.com/america/", "tags": ["a", "cst_ctn"]},
+    "tg": {
+        "url": "https://www.theguardian.com/uk/",
+        "tags": ["div", "fc-item__container"],
+    },
+}
+
+uk_dict = {
+    "aj": {
+        "url": "https://www.aljazeera.com/where/united-kingdom/",
+        "tags": ["article", "gc"],
+    },
+    "ib": {
+        "url": "https://www.infobae.com/tag/reino-unido/",
+        "tags": ["a", "nd-feed-list-card"],
+    },
+    "tg": {
+        "url": "https://www.theguardian.com/uk-news/",
+        "tags": ["div", "fc-item__container"],
+    },
+}
+
+mx_la_dict = {
+    "aj": {
+        "url": "https://www.aljazeera.com/latin-america/",
+        "tags": ["article", "gc"],
+    },
+    "ib": {"url": "https://www.infobae.com/america/mexico/", "tags": ["a", "cst_ctn"]},
+    "tg": {
+        "url": "https://www.theguardian.com/world/mexico+americas/",
+        "tags": ["div", "fc-item__container"],
+    },
+}
+
+
+world_dict = {
+    "aj": {"url": "https://www.aljazeera.com/news/", "tags": ["article", "gc"]},
+    "ib": {
+        "url": "https://www.infobae.com/america/mundo/",
+        "tags": ["a", "nd-feed-list-card"],
+    },
+    "tg": {
+        "url": "https://www.theguardian.com/world/",
+        "tags": ["div", "fc-item__container"],
+    },
+}
+
+sci_dict = {
+    "aj": {
+        "url": "https://www.aljazeera.com/tag/science-and-technology/",
+        "tags": ["article", "gc"],
+    },
+    "ib": {
+        "url": "https://www.infobae.com/america/ciencia-america/",
+        "tags": ["a", "nd-feed-list-card"],
+    },
+    "tg": {
+        "url": "https://www.theguardian.com/science/",
+        "tags": ["div", "fc-item__container"],
+    },
+}
+
+tech_dict = {
+    "aj": {
+        "url": "https://www.aljazeera.com/tag/technology/",
+        "tags": ["article", "gc"],
+    },
+    "ib": {
+        "url": "https://www.infobae.com/america/tecno/",
+        "tags": ["a", "nd-feed-list-card"],
+    },
+    "tg": {
+        "url": "https://www.theguardian.com/uk/technology/",
+        "tags": ["div", "fc-item__container"],
+    },
+}
+
+sports_dict = {
+    "aj": {"url": "https://www.aljazeera.com/sports/", "tags": ["article", "gc"]},
+    "ib": {
+        "url": "https://www.infobae.com/america/deportes/",
+        "tags": ["a", "nd-feed-list-card"],
+    },
+    "tg": {
+        "url": "https://www.theguardian.com/uk/sport/",
+        "tags": ["div", "fc-item__container"],
+    },
+}
+
+
+culture_dict = {
+    "aj": {
+        "url": "https://www.aljazeera.com/tag/arts-and-culture/",
+        "tags": ["article", "gc"],
+    },
+    "ib": {
+        "url": "https://www.infobae.com/america/cultura/",
+        "tags": ["a", "nd-feed-list-card"],
+    },
+    "tg": {
+        "url": "https://www.theguardian.com/uk/culture/",
+        "tags": ["div", "fc-item__container"],
+    },
+}
+
+photos_dict = {
+    "aj": {
+        "url": "https://www.aljazeera.com/gallery/",
+        "tags": ["article", "gc"],
+    },
+    "ib": {
+        "url": "https://www.infobae.com/america/fotos/",
+        "tags": ["a", "nd-feed-list-card"],
+    },
+    "tg": {
+        "url": "https://www.theguardian.com/inpictures/",
+        "tags": ["div", "fc-item__container"],
+    },
+}
